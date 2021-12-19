37567fcc057e5f83a22bb006990e5f131052088b
volmaticmw5 <volmaticmw5@gmail.com>
2021-12-19 15:27:12 +0000

    Weapon stuff

---
+++ Scripts/Weapon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Godot;
+
+public class Weapon : StaticBody
+{
+    [Export] public string Name;
+    [Export] public string Description;
+    [Export] public float Damage;
+    [Export] public float AttackSpeed;
+
+    public void _on_PickupArea_area_entered()
+    {
+        GD.Print("Entered");
+    }
+
+    public void _on_PickupArea_body_entered()
+    {
+        GD.Print("Body enter");
+    }
+}
+
